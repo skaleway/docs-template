d49f2d2c843ba48bff4dfb3161965d9213140c61
code-env <org.codeen@gmail.com>
2024-09-17 13:29:28 +0100

    feat: initialized the project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+import { withContentlayer } from "next-contentlayer2";
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withContentlayer(nextConfig);
